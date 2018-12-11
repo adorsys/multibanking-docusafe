ca1fff28e91b4234c64f1ec0fb2f46e6260dfd54
pspex <pspex@adorsys.de>
2018-12-11 11:20:32 +0100

    MUL-321 dependencies still to snapshots, because sts and docusafe have not been build yet. will fail in travis at this time

--- multibanking-rest/src/main/java/de/adorsys/multibanking/service/base/CacheBasedService.java
+++ multibanking-rest/src/main/java/de/adorsys/multibanking/service/base/CacheBasedService.java
@@ -29,7 +29,7 @@ import java.util.Optional;
  * @author fpo 2018-04-06 04:36
  *
  */
-public abstract class CacheBasedService implements g {
+public abstract class CacheBasedService implements CacheBasedInterface {
     private final static Logger LOGGER = LoggerFactory.getLogger(CacheBasedService.class);
 
 	private ObjectMapper objectMapper;
